92b0e1f7be25cd0cb60c66b8576cb3d7bbe3b54d
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-01 07:22:08 +0300

    design recommended component

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,7 +1,18 @@
 import "./Recommended.css";
 
 const Recommended = () => {
-  return <div>Recommended</div>;
+  return (
+    <div>
+      <h2 className="recommended__title">Recommended</h2>
+      <div className="recommended">
+        <button className="btns">All Products</button>
+        <button className="btns">Nike</button>
+        <button className="btns">Adidas</button>
+        <button className="btns">Puma</button>
+        <button className="btns">Vans</button>
+      </div>
+    </div>
+  );
 };
 
 export default Recommended;
