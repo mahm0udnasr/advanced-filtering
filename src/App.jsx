9ce204577ea269e81968f79a139e324aefbc1409
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-01 06:00:27 +0300

    structure preparation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Navigation from "./Navigation/Navigation";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+
 function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
-  )
+    <>
+      <Navigation />
+      <Products />
+      <Recommended />
+    </>
+  );
 }
 
-export default App
+export default App;
