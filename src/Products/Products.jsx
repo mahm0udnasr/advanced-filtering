615985ca397e8410320d4f73d77a693c1b63b8bc
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-01 06:50:04 +0300

    design a product component

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,7 +1,35 @@
+import data from "../db/data";
+import { BsFillBagHeartFill } from "react-icons/bs";
 import "./Products.css";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <section className="card-container">
+        <section className="card">
+          <img src={data[0].img} alt={data[0].title} className="card-img" />
+          <div className="card-details">
+            <h3 className="card-title">{data[0].title}</h3>
+            <section className="card-reviews">
+              {data[0].star}
+              {data[0].star}
+              {data[0].star}
+              {data[0].star}
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>{data[0].prevPrice}</del> {data[0].newPrice}
+              </div>
+              <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Products;
