7b65f01cf42fd2ffe5ad652aaabe46c086facead
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-01 06:11:29 +0300

    design a navigation

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,7 +1,30 @@
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 import "./Navigation.css";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Entre your search shoes."
+        />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
