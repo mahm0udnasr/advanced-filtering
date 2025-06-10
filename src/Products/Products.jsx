ce585dbb2902a1e70e986da906ac3ba9347f9256
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-10 11:21:09 +0300

    add category component

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -27,6 +27,75 @@ const Products = () => {
             </section>
           </div>
         </section>
+        {/* start */}
+        <section className="card">
+          <img src={data[0].img} alt={data[0].title} className="card-img" />
+          <div className="card-details">
+            <h3 className="card-title">{data[0].title}</h3>
+            <section className="card-reviews">
+              {data[0].star}
+              {data[0].star}
+              {data[0].star}
+              {data[0].star}
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>{data[0].prevPrice}</del> {data[0].newPrice}
+              </div>
+              <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+              </div>
+            </section>
+          </div>
+        </section>
+
+                <section className="card">
+          <img src={data[0].img} alt={data[0].title} className="card-img" />
+          <div className="card-details">
+            <h3 className="card-title">{data[0].title}</h3>
+            <section className="card-reviews">
+              {data[0].star}
+              {data[0].star}
+              {data[0].star}
+              {data[0].star}
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>{data[0].prevPrice}</del> {data[0].newPrice}
+              </div>
+              <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+              </div>
+            </section>
+          </div>
+        </section>
+
+                <section className="card">
+          <img src={data[0].img} alt={data[0].title} className="card-img" />
+          <div className="card-details">
+            <h3 className="card-title">{data[0].title}</h3>
+            <section className="card-reviews">
+              {data[0].star}
+              {data[0].star}
+              {data[0].star}
+              {data[0].star}
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>{data[0].prevPrice}</del> {data[0].newPrice}
+              </div>
+              <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+              </div>
+            </section>
+          </div>
+        </section>
+        {/* end */}
+
+
       </section>
     </>
   );
