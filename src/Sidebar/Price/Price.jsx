921905a405e034e9e1549c64dc5c8407525b96d9
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-11 05:40:27 +0300

    design price component

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,7 +1,33 @@
 import "./Price.css";
 
 const Price = () => {
-  return <div>Price</div>;
+  return (
+    <div className="ml">
+      <h2 className="sidebar-title price-title">Price</h2>
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name="category" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="category" />
+          <span className="checkmark"></span>$0 - $50
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="category" />
+          <span className="checkmark"></span>$50 - $100
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="category" />
+          <span className="checkmark"></span>$100 - $150
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="category" />
+          <span className="checkmark"></span>Over $150
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Price;
