e4afd51b3891b14841ad8f8eb18e56104d29ea0e
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-11 05:44:20 +0300

    design colors component

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -6,23 +6,23 @@ const Price = () => {
       <h2 className="sidebar-title price-title">Price</h2>
       <div>
         <label className="sidebar-label-container">
-          <input type="radio" name="category" />
+          <input type="radio" name="price" />
           <span className="checkmark"></span>All
         </label>
         <label className="sidebar-label-container">
-          <input type="radio" name="category" />
+          <input type="radio" name="price" />
           <span className="checkmark"></span>$0 - $50
         </label>
         <label className="sidebar-label-container">
-          <input type="radio" name="category" />
+          <input type="radio" name="price" />
           <span className="checkmark"></span>$50 - $100
         </label>
         <label className="sidebar-label-container">
-          <input type="radio" name="category" />
+          <input type="radio" name="price" />
           <span className="checkmark"></span>$100 - $150
         </label>
         <label className="sidebar-label-container">
-          <input type="radio" name="category" />
+          <input type="radio" name="price" />
           <span className="checkmark"></span>Over $150
         </label>
       </div>
